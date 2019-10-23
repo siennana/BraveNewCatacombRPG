ab0a2955d0c6e498f90e6c2c7f7f69e670ab7fc3
johnyu763 <johny11@uci.edu>
2019-10-22 19:34:12 -0700

    Unity Chan Walk Animation

--- Assets/Moving.cs
+++ Assets/Moving.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Moving : MonoBehaviour
 {
     public float _rotationSpeed = 180;
+    private Animator anim;
 
     public Vector3 rotation;
     // Start is called before the first frame update
     void Start()
     {
         this.rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * _rotationSpeed * Time.deltaTime, 0);
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,6 +20,7 @@ public class Moving : MonoBehaviour
     {
         transform.Translate(0f, 0f, 3 * Input.GetAxis("Vertical") * Time.deltaTime);
         transform.Rotate(0f, Input.GetAxisRaw("Horizontal") * _rotationSpeed * Time.deltaTime, 0f);
+        anim.SetFloat("Speed", Input.GetAxisRaw("Vertical"));
     }
 
     public void MoveForward()
