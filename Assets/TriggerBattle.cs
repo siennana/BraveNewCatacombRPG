21b83811744c4d324aea5ea57a795189de9692c6
johnyu763 <johny11@uci.edu>
2019-10-29 17:18:00 -0700

    Unload Terrain when entering battle

--- Assets/TriggerBattle.cs
+++ Assets/TriggerBattle.cs
@@ -16,7 +16,8 @@ public class TriggerBattle : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     { 
         Debug.Log("Oh you're approaching me!");
-        SceneManager.LoadScene("Battle", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Battle", LoadSceneMode.Single);
+
         //Debug.Log("Scene2 loading: " + scenePaths[0]);
         //SceneManager.LoadScene(scenePaths[0], LoadSceneMode.Single);
 
