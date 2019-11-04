51cd19bfa57a7e4bb004ad4a86877cdbc8ce7866
johnyu763 <johny11@uci.edu>
2019-11-04 14:42:21 -0800

    Basic Battle

---
+++ Assets/LoadLevel1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel1 : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene("Level 1", LoadSceneMode.Single);
+    }
+}
