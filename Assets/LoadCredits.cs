f044a169206e5125ea842a7adf183687a258fbb0
johnyu763 <johny11@uci.edu>
2019-11-06 09:42:34 -0800

    Credit screen

---
+++ Assets/LoadCredits.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadCredits : MonoBehaviour
+{
+    public GameObject mainMenu;
+
+    public void LoadCreditScreen()
+    {
+        mainMenu.SetActive(false);
+        this.gameObject.SetActive(true);
+    }
+
+    public void LoadMainScreen()
+    {
+        mainMenu.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
