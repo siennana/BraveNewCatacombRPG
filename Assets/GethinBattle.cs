a3253c45804e52781a93ad37837723b215beea6b
johnyu763 <johny11@uci.edu>
2019-11-20 18:29:30 -0800

    Semi-working gethin

---
+++ Assets/GethinBattle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GethinBattle : MonoBehaviour
+{
+    private Animator anim;
+    public GameObject menuItems;
+    public GameObject attackItems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.enabled = true;
+    }
+
+    public void AttackAction()
+    {
+        menuItems.SetActive(false);
+        attackItems.SetActive(false);
+        StartCoroutine(VillainAttack());
+
+    }
+
+    IEnumerator VillainAttack()
+    {
+        yield return new WaitForSeconds(0.5f);
+        anim.Play("SwordAttack");
+        yield return new WaitForSeconds(1.7f);
+        menuItems.SetActive(true);
+    }
+}
